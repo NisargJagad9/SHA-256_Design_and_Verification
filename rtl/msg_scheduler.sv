97d5af8343ce1acf9b96a4bd953420291f501a94
“Nisarg <nisargjagad69@gmail.com>
2026-01-27 13:26:48 +0530

    Updated msg_scheduler

--- rtl/msg_scheduler.sv
+++ rtl/msg_scheduler.sv
@@ -20,14 +20,49 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 module msg_sch (
-                    input         clk,
-                    input         rst,
-                    input [31:0]  M_i,
-                    input         M_dv,
-                    output [31:0] W_o,
-                    output        W_dv
+                    input  logic        clk,
+                    input  logic        rst,
+                    input  logic [31:0] M_i,
+                    input  logic        M_dv,
+                    output logic [31:0] W_o,
+                    output logic        W_dv
 );
 
 
+logic [5:0] W_count;
+logic [31:0] W_reg [0:15];
+
+always_ff@(posedge clk or negedge rst)
+begin
+    if(!rst)
+    begin
+        W_count <= 'b0;
+        W_o <= 'b0;
+        W_dv <= 'b0;
+    end
+    else if(M_dv && W_count < 16)
+    begin
+        W_reg[W_count] <= M_i;
+        W_count++;
+        W_o <= W_reg[W_count];
+        W_dv <= 1'b1;
+    end
+    else if(W_count >= 16)
+    begin
+        
+        for (integer i = 0; i < 15 ; i++ ) begin
+            W_reg[i] <= W_reg[i+1];            
+        end
+        W_reg[15] <= W_o;
+        W_o <= sigma1(W_reg[14]) + W_reg[9] + sigma0(W_reg[1]) + W_reg[0];
+
+        if(W_count==63)
+        begin
+            W_count <= 'b0;
+            W_dv <= 1'b1;
+            W_o <= 'b0;
+        end
+    end
+end
 
 endmodule
