a1927761004a8eb84ec0993dbb33150893c3d0eb
“Nisarg <nisargjagad69@gmail.com>
2026-01-27 17:50:42 +0530

    python file changed to view W values

--- SHA256.py
+++ SHA256.py
@@ -157,4 +157,4 @@ def _rotate_right(num: int, shift: int, size: int = 32):
     return (num >> shift) | (num << size - shift)
 
 if __name__ == "__main__":
-    print(generate_hash("Hello").hex())
+    print(generate_hash("shravani").hex())
